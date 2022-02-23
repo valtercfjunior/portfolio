d3566bf2d5c8921f0cfdadf116ef5d8850368a58
Valter Junior <79730793+valtercfjunior@users.noreply.github.com>
2022-02-22 23:43:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+ Estudo de CSS, JS - API Github
---
+++ views/script.js
@@ -0,0 +1,38 @@
+const user = "valtercfjunior"
+const url = "https://api.github.com/users/"
+
+
+
+async function getUserData(){
+
+    let profileRep = url + user + '/repos'
+    await fetch(profileRep).then(res=>res.json()).then(data=>{
+     
+        let container = '';
+           
+
+        data.forEach(repo => {
+            container += `
+            <div id="projects" class="cards-laterais cards">
+                <h1> <img src="../public/assets/folder.svg" alt="folder"> ${repo.name}</h1>
+                <p>${repo.description}</p>
+                <div class="cardsStats">
+                    <img src="../public/assets/star.svg" alt="star">
+                    <p>${repo.stargazers_count}</p>
+                    <img src="../public/assets/git-branch.svg" alt="branch">
+                    <p>${repo.forks_count}</p>
+                </div>
+                </div>      
+            `
+             
+        });
+
+        document.getElementById('projectsContainer').innerHTML = container
+    })
+    
+       
+}
+
+getUserData()
+
+
